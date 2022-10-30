93ac20e34055c06f8ca67f9ccddcbe7066f1806f
Abhinav Singh <abhisinghbhu55@gmail.com>
2022-10-30 06:31:01 +0530

    final done

--- BankingApplication/src/main/java/com/project/bank/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.bank.repository;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import com.project.bank.model.Users;
-
-public interface UserRepository extends ReactiveMongoRepository<Users, Integer> {
-
-}
